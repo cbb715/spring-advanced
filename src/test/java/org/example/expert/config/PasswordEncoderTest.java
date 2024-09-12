b8f4b794dabe593a5e19d87a269efb92c39d245f
“cbb715” <iseul1121@gmail.com>
2024-09-12 11:29:50 +0900

    레벨 2-5 : 테스트 연습 - 1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -13,6 +13,7 @@ class PasswordEncoderTest {
     @InjectMocks
     private PasswordEncoder passwordEncoder;
 
+    // 레벨 2-5 테스트 연습 - 1
     @Test
     void matches_메서드가_정상적으로_동작한다() {
         // given
@@ -20,7 +21,7 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
